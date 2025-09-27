b2f044e1ebf24e4aab8292dcc5ea15903f163f18
adamxu29 <adam.xu290@gmail.com>
2025-09-27 14:16:39 -0600

    update distance sensor reset function

--- include/Lib-Eclipse/Chassis/Odometry.hpp
+++ include/Lib-Eclipse/Chassis/Odometry.hpp
@@ -4,6 +4,30 @@ extern double robot_theta;
 
 namespace Eclipse{
     class Odom{
+        private:
+            // odometry variables
+            double x = 0;
+            double y = 0;
+
+            // reset variables
+            double right_weightings = 0;
+            double back_weightings = 0;
+
+            double right_weighted_distance = 0;
+            double back_weighted_distance = 0;
+
+            const double mm_to_inches = 0.0393701;
+
+            const double right_offset = 30;
+            const double back_offset = 10;
+
+            // retune values for skills
+            const double max_x = 40.1124;
+            const double max_y = 5.7999;
+            const double min_x = -98.527;
+            const double min_y = -133.19;
+
+            std::pair<int, double> get_wall(double heading, double distance, bool horizontal);
         public:
 
             float horizontal_wheel_diameter;
@@ -19,29 +43,9 @@ namespace Eclipse{
             float get_left_displacement();
             float get_right_displacement();
 
-            double x = 0;
-            double y = 0;
-
             void update_position();
             void update_position_single_vertical();
 
-            double right_weightings = 0;
-            double back_weightings = 0;
-
-            double right_weighted_distance = 0;
-            double back_weighted_distance = 0;
-
-            const double mm_to_inches = 0.0393701;
-
-            const double right_offset = 30;
-            const double back_offset = 10;
-
-            // figure out/tune these values
-            const double max_x = 40.1124;
-            const double max_y = 5.7999;
-            const double min_x = -98.527;
-            const double min_y = -133.19;
-
-            void distance_sensor_reset(int wall_1, int wall_2, int readings);
+            void distance_sensor_reset(int readings, bool create_task = true);
     };
 }
--- src/Lib-Eclipse/Chassis/Odometry.cpp
+++ src/Lib-Eclipse/Chassis/Odometry.cpp
@@ -101,63 +101,101 @@ void Odom::update_position_single_vertical(){
     gui.update_match_checklist();
 }
 
-void Odom::distance_sensor_reset(int wall_1, int wall_2, int readings){
+std::pair<int, int> Eclipse::Odom::get_wall(double heading) {
+    // Normalize heading to [0, 360)
+    while (heading < 0) heading += 360;
+    while (heading >= 360) heading -= 360;
+
+    if(heading >= 315 || heading < 45){
+        // facing 0°
+        return {1, 2}; // right wall, back wall
+    }
+    else if((heading >= 45 || heading < 135)){
+        // facing 90°
+        return {2, 3}; // back wall, left wall
+    }  
+    else if((heading >= 135 || heading < 225)){
+        // facing 180°
+        return {3, 4}; // left wall, front wall
+    }
+    else if((heading >= 225 || heading < 315)){
+        // facing 270°
+        return {4, 1}; // front wall, right wall
+    }
+}
+
+
+void Odom::distance_sensor_reset(int readings, bool create_task){
     pros::Task task([&]() {
-        while(readings > 0){
-            double distance_1 = (right_sensor.get() - right_offset) * mm_to_inches;
-            double distance_2 = (back_sensor.get() - back_offset) * mm_to_inches;
-    
-            double confidence_1 = right_sensor.get_confidence();
-            double confidence_2 = back_sensor.get_confidence();
-    
-            right_weightings += confidence_1;
-            right_weighted_distance += distance_1 * confidence_1;
-    
-            back_weightings += confidence_2;
-            back_weighted_distance += distance_2 * confidence_2;
-    
-            pros::delay(10);
-            readings--;
-        }
-    
-        double avg_right_distance = right_weighted_distance / right_weightings;
-        double avg_back_distance = back_weighted_distance / back_weightings;
-    
-        char buffer[300];
-        sprintf(buffer, "Prev X: %.2f Y: %.2f", util.get_robot_x(), util.get_robot_y());
-        lv_label_set_text(gui.debug_line_5, buffer);
-    
-        switch(wall_1){
-            case 0:
-                util.set_robot_position(min_x + avg_right_distance, util.get_robot_y());
-                break;
-            case 1:
-                util.set_robot_position(util.get_robot_x(), max_y - avg_right_distance);
-                break;
-            case 2:
-                util.set_robot_position(max_x - avg_right_distance, util.get_robot_y());
-                break;
-            case 3:
-                util.set_robot_position(util.get_robot_x(), min_y + avg_right_distance);
-                break;
-        }
-    
-        switch(wall_2){
-            case 0:
-                util.set_robot_position(min_x + avg_back_distance, util.get_robot_y());
-                break;
-            case 1:
-                util.set_robot_position(util.get_robot_x(), max_y - avg_back_distance);
-                break;
-            case 2:
-                util.set_robot_position(max_x - avg_back_distance, util.get_robot_y());
-                break;
-            case 3:
-                util.set_robot_position(util.get_robot_x(), min_y + avg_back_distance);
-                break;
-        }
-    
-        sprintf(buffer, "New X: %.2f Y: %.2f", util.get_robot_x(), util.get_robot_y());
-        lv_label_set_text(gui.debug_line_6, buffer);
+        distance_sensor_reset(readings, false);
+        pros::delay(10);
+        return;
     });
+
+    while(readings > 0){
+        double distance_1 = (right_sensor.get() - right_offset) * mm_to_inches;
+        double distance_2 = (back_sensor.get() - back_offset) * mm_to_inches;
+
+        double confidence_1 = right_sensor.get_confidence();
+        double confidence_2 = back_sensor.get_confidence();
+
+        right_weightings += confidence_1;
+        right_weighted_distance += distance_1 * confidence_1;
+
+        back_weightings += confidence_2;
+        back_weighted_distance += distance_2 * confidence_2;
+
+        pros::delay(10);
+        readings--;
+    }
+
+    double avg_right_distance = right_weighted_distance / right_weightings;
+    double avg_back_distance = back_weighted_distance / back_weightings;
+
+    char buffer[300];
+    sprintf(buffer, "Prev X: %.2f Y: %.2f", util.get_robot_x(), util.get_robot_y());
+    lv_label_set_text(gui.debug_line_5, buffer);
+
+    auto [wall_1, wall_2] = get_wall(util.get_heading());
+
+    switch(wall_1){
+        case 1:
+            // 0° wall
+            util.set_robot_position(util.get_robot_x(), max_y - avg_right_distance);
+            break;
+        case 2:
+            // 90° wall
+            util.set_robot_position(max_x - avg_right_distance, util.get_robot_y());
+            break;
+        case 3:
+            // 180° wall
+            util.set_robot_position(util.get_robot_x(), min_y + avg_right_distance);
+            break;
+        case 4:
+            // 270° wall
+            util.set_robot_position(min_x + avg_right_distance, util.get_robot_y());
+            break;
+    }
+
+    switch(wall_2){
+        case 1:
+            // 0° wall
+            util.set_robot_position(util.get_robot_x(), max_y - avg_right_distance);
+            break;
+        case 2:
+            // 90° wall
+            util.set_robot_position(max_x - avg_right_distance, util.get_robot_y());
+            break;
+        case 3:
+            // 180° wall
+            util.set_robot_position(util.get_robot_x(), min_y + avg_right_distance);
+            break;
+        case 4:
+            // 270° wall
+            util.set_robot_position(min_x + avg_right_distance, util.get_robot_y());
+            break;
+    }
+
+    sprintf(buffer, "New X: %.2f Y: %.2f", util.get_robot_x(), util.get_robot_y());
+    lv_label_set_text(gui.debug_line_6, buffer);
  }
